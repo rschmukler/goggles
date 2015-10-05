7ff1736b77ae57e8489f30b75c5ce3a82dacd687
Ryan Schmukler <rschmukler@gmail.com>
2015-10-04 23:39:52 -0400

    Add more animations, button logic

--- main.cc
+++ main.cc
@@ -1,82 +1,133 @@
+#include <Button.h>
 #include <Adafruit_NeoPixel.h>
 
-const int BUTTON = 1;
-const int LENS = 0;
-const int LONG_HOLD_TIME = 2000;
-const int NUM_PIXELS = 16;
-
-int buttonPressed = 0;
-int lastButtonState = 0;
-int ledState = LOW;
-int buttonPressStartTime;
-bool longHoldAlreadyTriggered = false;
+const int BUTTON = 0;
+const int LENS = 1;
+const int NUM_PIXELS = 32;
+const int MAX_BRIGHTNESS = 100;
+const int MIN_BRIGHTNESS = 5;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, LENS);
+Button button = Button(BUTTON, false, false, 25);
+
+uint32_t currentColor = 0x9B0DBE;
+uint8_t currentWheelPos = 0;
+uint8_t currentBrightness = MIN_BRIGHTNESS;
+bool breathingUp = true;
+
+const unsigned long INTERVAL = 25;
+unsigned long previousMillis = 0;
+
+enum MODE {
+  modeSteady, modeBreath, modeRainbowCycle
+} currentMode = modeSteady;
+
+
 
 void setup() {
-  uint32_t startColor = 0x9B0DBE;
   // put your setup code here, to run once:
-  pinMode(BUTTON, INPUT);
   pixels.begin();
-  pixels.setBrightness(255);
-  setAllPixels(startColor);
+  pixels.setBrightness(currentBrightness);
+  setAllPixels(currentColor);
   pixels.show();
 }
 
 void setAllPixels(uint32_t color) {
+  currentColor = color;
   for(int i = 0; i < NUM_PIXELS; ++i) {
     pixels.setPixelColor(i, color);
   }
+  pixels.show();
+}
+
+void flash() {
+  uint32_t restoreColor = currentColor;
+  pixels.setBrightness(254);
+  setAllPixels(random(0, 0xffffff));
+  delay(250);
+  pixels.setBrightness(currentBrightness);
+  setAllPixels(restoreColor);
+}
+
+void cycleColor() {
+  if (!canAct()) return;
+  currentWheelPos += 1;
+  if (currentWheelPos > 255) currentWheelPos = 0;
+  setAllPixels(Wheel(currentWheelPos));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  checkButton();
-  if (longHoldStart()) {
-//    blinkLED();
-  }
-  if (longHoldRelease()) {
-//    toggleLED();
-  }
-  pixels.show();
-  lastButtonState = buttonPressed;
-}
-
-//void blinkLED() {
-//  int restoreLed = ledState;
-//  digitalWrite(LED, LOW);
-//  delay(300); 
-//  digitalWrite(LED, HIGH);
-//  digitalWrite(LED, restoreLed);
-//}
-
-void checkButton() {
-  buttonPressed = digitalRead(BUTTON);
-  if (buttonPressed != lastButtonState) {
-    if (buttonPressed == HIGH) {
-      longHoldAlreadyTriggered = false;
-      buttonPressStartTime = millis();
+  button.read();
+  if (button.pressedFor(5000)) {
+    cycleColor();
+  } else if (button.wasPressed()) {
+    flash();
+  } else {
+    switch (currentMode) {
+      case modeBreath: breath(); break;
+      case modeRainbowCycle: rainbowCycle(25); break;
+      default: break;
     }
   }
+  
+  pixels.show();
 }
 
-bool longHoldStart() {
-  if (!longHoldAlreadyTriggered && buttonPressed == HIGH && (millis() - buttonPressStartTime) > LONG_HOLD_TIME) {
-    longHoldAlreadyTriggered = true;
+bool canAct() {
+  if (millis() - previousMillis > INTERVAL) {
+    previousMillis = millis();
     return true;
   }
   return false;
 }
 
-bool longHoldRelease() {
-  return buttonPressed != lastButtonState && (millis() - buttonPressStartTime) > LONG_HOLD_TIME;
+void breath() {
+  if (!canAct()) return;
+  if (currentBrightness <= MIN_BRIGHTNESS) breathingUp = true;
+  if (currentBrightness >= MAX_BRIGHTNESS) breathingUp = false;
+  currentBrightness += (breathingUp ? 1 : -1);
+  pixels.setBrightness(currentBrightness);
 }
 
-//void toggleLED() {
-//  if (ledState == HIGH) {
-//    ledState = LOW;
-//  } else {
-//    ledState = HIGH;
-//  }
-//  digitalWrite(LED, ledState); 
-//}
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
+    }
+    pixels.show();
+    delay(wait);
+  }
+}
+
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < pixels.numPixels(); i=i+3) {
+        pixels.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      pixels.show();
+
+      delay(wait);
+
+      for (int i=0; i < pixels.numPixels(); i=i+3) {
+        pixels.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
